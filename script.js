b7113fd6c1ca49418d74f6d24eb5e2f715d38f9c
Athitiya Panlabal <128718432+Fishshohappy@users.noreply.github.com>
2023-05-27 19:35:54 +0700

    create nav, light-dark mode, add inform and pic

---
+++ script.js
@@ -0,0 +1,35 @@
+const switchToggle = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+const nav = document.getElementById('nav');
+function switchMode(e) {
+    const a = nav.getElementsByTagName('a')
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode(a);
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        lightMode(a);
+    }
+}
+function darkMode(a) {
+    toggleIcon.children[0].textContent = "โหมดกลางคืน";
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    nav.style.backgroundColor = 'rgb(0 0 0/ 50%)';
+    // a.forEach(element => {
+    //     console.log(element)
+    // });
+    for (let index = 0; index < a.length; index++) {
+        const element = a[index];
+        element.style.color = 'var(--primary-color)';
+    }
+}
+function lightMode(a) {
+    toggleIcon.children[0].textContent = "โหมดกลางวัน";
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    nav.style.backgroundColor = 'rgb(255 255 255/ 50%)';
+    for (let index = 0; index < a.length; index++) {
+        const element = a[index];
+        element.style.color = 'var(--primary-color)';
+    }
+}
+switchToggle.addEventListener('change', switchMode);
